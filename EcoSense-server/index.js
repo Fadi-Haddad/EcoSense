ff1974d8c51ffe9fd14f5c7d58c32a24d9fb441d
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-21 22:05:45 +0300

    Create a listen function on deafult port

--- EcoSense-server/index.js
+++ EcoSense-server/index.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
 
 const port = process.env.PORT;
+
+app.listen(port, () => {
+     console.log(`Example app listening on port ${port}`)});
