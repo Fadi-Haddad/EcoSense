3231bd0c7c8f3d2227e06b45ac77103f98d9dce3
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-22 23:33:40 +0300

    Wrap getSensorReadings function in a try-catch block to handle errors

--- EcoSense-server/Controllers/sensor.controller.js
+++ EcoSense-server/Controllers/sensor.controller.js
@@ -1,7 +1,11 @@
 const sensorReading= require('../Models/sensor.readings.model');
 
 const getSensorReadings = async (req, res) => {
+    try{
         const {AQI,CO,CO2,Temp,Humidity} = req.body;
         const newReading = new sensorReading({AQI,CO,CO2,Temp,Humidity});
         await newReading.save();
+    } catch{
+        console.error(err);
+    }
     };
