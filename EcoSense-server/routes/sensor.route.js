d4238fe70f6d553f75a04153dcc880c9cee46ca4
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-23 00:28:01 +0300

    Create a path for getSensorReadings function

--- EcoSense-server/routes/sensor.route.js
+++ EcoSense-server/routes/sensor.route.js
@@ -2,5 +2,5 @@ const express = require('express');
 const { getSensorReadings } = require("../Controllers/sensor.controller");
 
 const router = express.Router();
-
+router.post("/", getSensorReadings);
 module.exports = router;
