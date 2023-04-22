30e0f364a7ed0db6839152d52bfcb55e8ef42fc1
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-22 13:29:18 +0300

    Add userType field in user.model

--- EcoSense-server/Models/user.model.js
+++ EcoSense-server/Models/user.model.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
-    
+    userType:{
+        type: String,
+        required: true,
+        enum : ['admin', 'user'],
+    },
 })
