0aed9c79809e876bb3817ea479a20dbaa12d30e4
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-23 01:36:48 +0300

    Import secrets.h to main.cpp and add it to .gitignore file

--- EcoSense-server/Arduino/main.cpp
+++ EcoSense-server/Arduino/main.cpp
@@ -1,3 +1,4 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266HTTPClient.h>
+#include "secrets.h"
--- EcoSense-server/Arduino/secrets.h
+++
@@ -1,8 +0,0 @@
-#ifndef SECRETS_H
-#define SECRETS_H
-
-const char* ssid = "";
-const char* password = "";
-const char* serverUrl = "";
-
-#endif
