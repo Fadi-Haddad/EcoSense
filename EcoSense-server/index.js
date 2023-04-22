135b00cfdb64664807f4565ef268aef7ad77cc04
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-22 12:11:23 +0300

    Fetch database URI from .env file

--- EcoSense-server/index.js
+++ EcoSense-server/index.js
@@ -7,5 +7,7 @@ const port = process.env.PORT;
 
 app.listen(port, () => {
      console.log(`app listening on port ${port}`)});
-     
+
 const mongoose = require("mongoose");
+
+const MONGODB_URI = process.env.MONGODB_URI;
