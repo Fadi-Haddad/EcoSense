8271abbcd3e48250b4661bb4d98551f31b36dd6d
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-22 23:23:41 +0300

    Create new sensorReading object

--- EcoSense-server/Controllers/sensor.controller.js
+++ EcoSense-server/Controllers/sensor.controller.js
@@ -2,4 +2,5 @@ const sensorReading= require('../Models/sensor.readings.model');
 
 const getSensorReadings = (req, res) => {
         const {AQI,CO,CO2,Temp,Humidity} = req.body;
+        const newReading = new sensorReading({AQI,CO,CO2,Temp,Humidity});
     };
