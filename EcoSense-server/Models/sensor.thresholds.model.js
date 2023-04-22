b3cc4b7539d08f90a95b43f7d620c1931cd735cb
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-22 16:21:48 +0300

    Add maxValue field in sensor.thresholds.model

--- EcoSense-server/Models/sensor.thresholds.model.js
+++ EcoSense-server/Models/sensor.thresholds.model.js
@@ -2,5 +2,8 @@ const mongoose = require('mongoose');
 const sensorThresholdsSchema = new mongoose.Schema({
 sensor:{
     name:String,
-    required:true},
+    required:true,},
+maxValue:{
+    type: Number,
+    required: true,},
 })
