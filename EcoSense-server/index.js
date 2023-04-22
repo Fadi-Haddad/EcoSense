b6c939726cd8bf911990ffbba8b24c9bcf8070cd
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-22 12:14:13 +0300

    Add a function to connect to database

--- EcoSense-server/index.js
+++ EcoSense-server/index.js
@@ -11,3 +11,7 @@ app.listen(port, () => {
 const mongoose = require("mongoose");
 
 const MONGODB_URI = process.env.MONGODB_URI;
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
