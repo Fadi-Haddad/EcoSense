213efeb34b59e0bd11fc836166e6d7fa4ce1cf0a
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-23 18:05:41 +0300

    Require getSensorReadings function form sensor.controller

--- EcoSense-server/routes/sensor.route.js
+++ EcoSense-server/routes/sensor.route.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { saveSensorReadings } = require("../Controllers/sensor.controller");
+const { saveSensorReadings, getSensorReadings } = require("../Controllers/sensor.controller");
 
 const router = express.Router();
 router.post("/save", saveSensorReadings);
