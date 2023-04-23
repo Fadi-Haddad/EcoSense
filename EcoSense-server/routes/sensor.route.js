b71ac19b7c3ac0501694800cb9f1fe589abacb69
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-24 00:49:26 +0300

    Import getSensorsState function from sensor.controller

--- EcoSense-server/routes/sensor.route.js
+++ EcoSense-server/routes/sensor.route.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { saveSensorReadings, getSensorReadings,getSensorMinReading,getSensorMaxReading } = require("../Controllers/sensor.controller");
+const { saveSensorReadings, getSensorReadings,getSensorMinReading,getSensorMaxReading,getSensorsState } = require("../Controllers/sensor.controller");
 
 const router = express.Router();
 router.post("/save", saveSensorReadings);
 router.get("/get", getSensorReadings);
-router.get("/get_sensors_state", getSensorState);
+router.get("/get_sensors_state", getSensorsState);
 router.get("/get/:sensor_name/min", getSensorMinReading);
 router.get("/get/:sensor_name/max", getSensorMaxReading);
 module.exports = router;
