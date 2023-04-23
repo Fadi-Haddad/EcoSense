7e1e6d7b05fc1fb9eda581c6bb2d64c2ea4ef051
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-23 20:54:09 +0300

    Require getSensorMinReading function from sensor.controller

--- EcoSense-server/routes/sensor.route.js
+++ EcoSense-server/routes/sensor.route.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { saveSensorReadings, getSensorReadings } = require("../Controllers/sensor.controller");
+const { saveSensorReadings, getSensorReadings,getSensorMinReading } = require("../Controllers/sensor.controller");
 
 const router = express.Router();
 router.post("/save", saveSensorReadings);
