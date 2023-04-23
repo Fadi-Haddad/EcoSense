9322485a21c15b0d57987815d2d23de85bcb99c2
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-23 18:00:59 +0300

    Create a new route for getSensorReadings function

--- EcoSense-server/routes/sensor.route.js
+++ EcoSense-server/routes/sensor.route.js
@@ -3,4 +3,5 @@ const { saveSensorReadings } = require("../Controllers/sensor.controller");
 
 const router = express.Router();
 router.post("/save", saveSensorReadings);
+router.post("/get", getSensorReadings);
 module.exports = router;
