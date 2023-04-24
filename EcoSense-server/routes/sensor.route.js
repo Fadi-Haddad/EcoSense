eb436944eabc36e8135effbe81bb1b29992fe5dc
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-24 13:41:53 +0300

    Backend: :sparkles: Add a route for setSensorMinReading function

--- EcoSense-server/routes/sensor.route.js
+++ EcoSense-server/routes/sensor.route.js
@@ -15,4 +15,5 @@ router.get("/get_sensors_state", getSensorsState);
 router.get("/set_sensors_state/:state", setSensorsState);
 router.get("/get/:sensor_name/min", getSensorMinReading);
 router.get("/get/:sensor_name/max", getSensorMaxReading);
+router.get("/set/:sensor_name/min", setSensorMinReading);
 module.exports = router;
