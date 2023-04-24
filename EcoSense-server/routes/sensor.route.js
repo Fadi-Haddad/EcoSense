dc03e0379c33b12b076a0532048e3dd9107b3a39
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-24 03:27:34 +0300

    Create new route to setSensorsState

--- EcoSense-server/routes/sensor.route.js
+++ EcoSense-server/routes/sensor.route.js
@@ -5,6 +5,7 @@ const router = express.Router();
 router.post("/save", saveSensorReadings);
 router.get("/get", getSensorReadings);
 router.get("/get_sensors_state", getSensorsState);
+router.get("/set_sensors_state/:state", setSensorsState);
 router.get("/get/:sensor_name/min", getSensorMinReading);
 router.get("/get/:sensor_name/max", getSensorMaxReading);
 module.exports = router;
