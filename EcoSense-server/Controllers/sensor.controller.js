4717e47b78e7ca56009f347eff569365d2718cce
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-24 03:51:01 +0300

    Fix an error in current_state value

--- EcoSense-server/Controllers/sensor.controller.js
+++ EcoSense-server/Controllers/sensor.controller.js
@@ -79,7 +79,7 @@ const getSensorsState = async (req, res) => {
         const new_state= req.params.state;
         if (validSensorStates.includes(new_state)){
             const current_state = await sensorsState.findOne();
-            if (new_state !== current_state){
+            if (new_state !== current_state.state){
                 await sensorsState.findOneAndUpdate({}, { state: new_state }, { new: true });
                 console.log("state changed to "+new_state);
                 res.status(200).json({ message: 'state changed' });}
