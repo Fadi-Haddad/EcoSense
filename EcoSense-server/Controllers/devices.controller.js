9c1ef858e92b602a3d03c24f98ed9b3e9f1c6b74
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-26 23:19:40 +0300

    Backend: Add a function to check if device name is valid

--- EcoSense-server/Controllers/devices.controller.js
+++ EcoSense-server/Controllers/devices.controller.js
@@ -5,8 +5,9 @@ const setDeviceState = async(req,res)=>{
     try{
     const device_state = req.params.state;
     const device_name = req.params.device;
-    const updateState= await Devices.findOneAndReplace({name:device_name},{state:device_state}, {new:true});
-    res.send(updateState);
+    if(validDeviceNames.includes(device_name)){
+        const updateState= await Devices.findOneAndReplace({name:device_name},{state:device_state}, {new:true});
+        res.send(updateState);}
     } catch(err){
         console.log(err);
         res.status(400).json({ message: 'Error update devices state' });
