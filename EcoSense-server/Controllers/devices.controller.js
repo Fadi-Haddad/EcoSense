47e6126926d7868942f6d9efe662d501f30db63b
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-26 22:53:11 +0300

    Backend: Update devices state

--- EcoSense-server/Controllers/devices.controller.js
+++ EcoSense-server/Controllers/devices.controller.js
@@ -1,6 +1,7 @@
-const Device = require("../Models/devices.model");
+const Devices = require("../Models/devices.model");
 
-const setDeviceState = (req,res)=>{
+const setDeviceState = async(req,res)=>{
     const state = req.params.state;
     const device = req.params.device;
+    const updateState= await Devices.findOneAndReplace({name:device_name},{state:device_state}, {new:true});
 }
