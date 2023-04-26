d1b90c79d289aa6f934537baa342d239dba8918e
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-26 22:59:55 +0300

    Backend::bug: Fix variables names

--- EcoSense-server/Controllers/devices.controller.js
+++ EcoSense-server/Controllers/devices.controller.js
@@ -2,9 +2,10 @@ const Devices = require("../Models/devices.model");
 
 const setDeviceState = async(req,res)=>{
     try{
-    const state = req.params.state;
-    const device = req.params.device;
-    const updateState= await Devices.findOneAndReplace({name:device_name},{state:device_state}, {new:true});  
+    const device_state = req.params.state;
+    const device_name = req.params.device;
+    const updateState= await Devices.findOneAndReplace({name:device_name},{state:device_state}, {new:true});
+    res.send(updateState);
     } catch(err){
         console.log(err);
         res.status(400).json({ message: 'Error update devices state' });
