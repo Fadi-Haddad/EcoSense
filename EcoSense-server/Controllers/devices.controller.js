c6b2834e5d06f1d24faeed6add539ad6c994df6f
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-26 22:56:43 +0300

    Backend: Return an error response if function fails

--- EcoSense-server/Controllers/devices.controller.js
+++ EcoSense-server/Controllers/devices.controller.js
@@ -6,7 +6,8 @@ const setDeviceState = async(req,res)=>{
     const device = req.params.device;
     const updateState= await Devices.findOneAndReplace({name:device_name},{state:device_state}, {new:true});  
     } catch(err){
-        cosole.log(err):
+        console.log(err);
+        res.status(400).json({ message: 'Error update devices state' });
     }
 
 }
