5ee34a7c5d0e7a036a13d30cb559f60313889676
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-26 20:50:47 +0300

    Backend: Export router as a module

--- EcoSense-server/routes/device.route.js
+++ EcoSense-server/routes/device.route.js
@@ -1,4 +1,6 @@
 const express = require('express');
 
 const router = express.Router();
-router.get("/set_device_state/:device/:state", getSensorReadings);
+router.get("/set_device_state/:device/:state", setDeviceState);
+
+module.exports  = router;
