26806092edf49f2edcfe41e4bf20eb3d59c2d828
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-27 23:48:37 +0300

    Backend: Calculate hash for password

--- EcoSense-server/Controllers/auth.controller.js
+++ EcoSense-server/Controllers/auth.controller.js
@@ -1,7 +1,8 @@
 const login = (req,res)=>{
 
 }
-const createUser = (req,res)=>{
+const createUser = async(req,res)=>{
     const { email, password, userType } = req.body;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
 }
 module.exports = {login};
