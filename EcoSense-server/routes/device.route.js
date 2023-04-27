f725ae2e02c5354ccfa28229292320229395393c
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-27 16:02:21 +0300

    Backend: Fixed parameters names

--- EcoSense-server/routes/device.route.js
+++ EcoSense-server/routes/device.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
 const router = express.Router();
-router.get("/set_device_state/:device/:state", setDeviceState);
+router.get("/set_fan_state/:fan_operation_mode/:fan_state", setFanState);
 
 module.exports  = router;
