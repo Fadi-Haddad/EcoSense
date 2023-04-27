27c851443dfc62423ec3a5a2cade4ca2963e9396
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-27 15:49:56 +0300

    Backend: Return success message when saving succeeds

--- EcoSense-server/Controllers/devices.controller.js
+++ EcoSense-server/Controllers/devices.controller.js
@@ -45,6 +45,7 @@ const setFanState = async (req, res) => {
     const updateState = await Devices.findOneAndUpdate({ name: 'fan' }, { state: fanState }, { new: true });
     if (!updateState) {
         return res.status(400).json({ message: 'Failed to update fan state.' });
-}
+        }
+    return res.status(200).json({ message: 'Device state updated successfully.' });
     }
 }
