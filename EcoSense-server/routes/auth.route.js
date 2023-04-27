a59f9e9e47c35d2ce9a335289f91c180ffbc8ada
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-27 23:10:24 +0300

    Backend: Require login function from auth.controller

--- EcoSense-server/routes/auth.route.js
+++ EcoSense-server/routes/auth.route.js
@@ -1,5 +1,7 @@
 const { Router } = require("express");
 const router  = Router();
+const {login,} = require("../Controllers/user.controller.js");
 
 router.get("/login",login);
 
+module.exports = router;
