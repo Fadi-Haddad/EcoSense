f600d00f746d8d2539f5d807e82a43fb53fcb11b
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-27 23:50:11 +0300

    Backend: Add Hash salt rounds

--- EcoSense-server/Controllers/auth.controller.js
+++ EcoSense-server/Controllers/auth.controller.js
@@ -3,6 +3,7 @@ const login = (req,res)=>{
 }
 const createUser = async(req,res)=>{
     const { email, password, userType } = req.body;
+    const saltRounds=10;
     const hashedPassword = await bcrypt.hash(password, saltRounds);
 }
 module.exports = {login};
