77b77a5bb2595d66f5e14666f3baa54655f2bc0e
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-28 00:04:27 +0300

    Backend: Return an error message if password or email are invalid

--- EcoSense-server/Controllers/auth.controller.js
+++ EcoSense-server/Controllers/auth.controller.js
@@ -2,6 +2,9 @@ const login = async (req,res)=>{
     const { email, password } = req.body;
     const user = await User.findOne({ email });
     const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid || !user) {
+        return res.status(401).json({ message: 'Invalid email or password' });
+      }
 
 }
 const createUser = async(req,res)=>{
