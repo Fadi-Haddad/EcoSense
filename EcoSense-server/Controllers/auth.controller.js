891619d37a0c06a5325457cca7b7bf84f9b1eb5e
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-27 23:53:20 +0300

    Backend: Created new user in the users table

--- EcoSense-server/Controllers/auth.controller.js
+++ EcoSense-server/Controllers/auth.controller.js
@@ -5,5 +5,7 @@ const createUser = async(req,res)=>{
     const { email, password, userType } = req.body;
     const saltRounds=10;
     const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const user = new User({ email, password: hashedPassword, userType });
+    await user.save();
 }
 module.exports = {login};
