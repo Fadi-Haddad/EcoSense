a517a426d2c49f9eccea4039dcbc0e11cd39d214
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-28 00:35:40 +0300

    Backend: Parse token to get token

--- EcoSense-server/middlewares/auth.middleware.js
+++ EcoSense-server/middlewares/auth.middleware.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
 const User = require('../Models/user.model');
 const authMiddleware = async (req, res, next) => {
-
+    const token = req.headers.authorization.split(' ')[1];
 };
