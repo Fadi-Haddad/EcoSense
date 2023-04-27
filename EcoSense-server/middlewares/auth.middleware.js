28e98f9d30d78fb75f4a89e4177cef5a65527607
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-28 00:36:57 +0300

    Backend: verify token

--- EcoSense-server/middlewares/auth.middleware.js
+++ EcoSense-server/middlewares/auth.middleware.js
@@ -2,4 +2,5 @@ const jwt = require('jsonwebtoken');
 const User = require('../Models/user.model');
 const authMiddleware = async (req, res, next) => {
     const token = req.headers.authorization.split(' ')[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 };
