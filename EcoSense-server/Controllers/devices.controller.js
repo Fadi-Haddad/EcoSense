17775f32dd5be73a9d68adf3fa0a2d2f3a04fa22
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-27 15:47:37 +0300

    Backend: Return an error message if saving fails

--- EcoSense-server/Controllers/devices.controller.js
+++ EcoSense-server/Controllers/devices.controller.js
@@ -43,4 +43,8 @@ const setFanState = async (req, res) => {
         fanState = fan.state;
         }
     const updateState = await Devices.findOneAndUpdate({ name: 'fan' }, { state: fanState }, { new: true });
-}}
+    if (!updateState) {
+        return res.status(400).json({ message: 'Failed to update fan state.' });
+}
+    }
+}
