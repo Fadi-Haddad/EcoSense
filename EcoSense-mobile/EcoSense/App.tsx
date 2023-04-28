e100f635d243a42f91daed70fb667e60e1b4dc90
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-29 00:09:20 +0300

    Frontend: Add an image to homepage

--- EcoSense-mobile/EcoSense/App.tsx
+++ EcoSense-mobile/EcoSense/App.tsx
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import LoginButton from './src/components/LoginButton';
 
 
 export default function App() {
   return (
     <View style={styles.container}>
+    <Image source={require('./assets/background.png')} style={{width: 100, height:100}}/>
       <LoginButton 
-      title='Log in'/>
+      title='Log in'
+      onPress={() => alert('Button clicked!')}/>
       <StatusBar style="auto" />
     </View>
   );
