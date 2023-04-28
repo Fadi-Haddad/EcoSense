edc294b67721a3ec823ce6c0d1aee61545554cec
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-28 09:33:53 +0300

    Backend: Store user property in the request and call next()

--- EcoSense-server/middlewares/auth.middleware.js
+++ EcoSense-server/middlewares/auth.middleware.js
@@ -4,4 +4,6 @@ const authMiddleware = async (req, res, next) => {
     const token = req.headers.authorization.split(' ')[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findOne({ email: decoded.email });
+    req.user = user;
+    next();
 };
