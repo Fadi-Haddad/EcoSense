8cfbe29e021c649aa4f6469cf67639ee6f2c2b22
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-28 09:36:42 +0300

    Backend: Wrap auth controller method code in a try catch block

--- EcoSense-server/middlewares/auth.middleware.js
+++ EcoSense-server/middlewares/auth.middleware.js
@@ -2,8 +2,12 @@ const jwt = require('jsonwebtoken');
 const User = require('../Models/user.model');
 const authMiddleware = async (req, res, next) => {
     const token = req.headers.authorization.split(' ')[1];
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findOne({ email: decoded.email });
-    req.user = user;
-    next();
-};
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const user = await User.findOne({ email: decoded.email });
+      if (!user) {
+        return res.status(401).json({ message: 'Unauthorized' });}
+      req.user = user;
+      next();} catch (error) {
+      return res.status(401).json({ message: 'Unauthorized' });}
+  };
