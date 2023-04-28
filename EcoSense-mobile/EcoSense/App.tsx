b7b3f2a76ddb87bf62c2d057e8832c1cbceb5eed
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-28 13:02:51 +0300

    Frontend: Import button components folder to App.tsx

--- EcoSense-mobile/EcoSense/App.tsx
+++ EcoSense-mobile/EcoSense/App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Button from './src/components/button';
 
 export default function App() {
   return (
--- EcoSense-mobile/EcoSense/src/components/button.tsx
+++ EcoSense-mobile/EcoSense/src/components/button.tsx
@@ -1,2 +1,22 @@
 import * as React from "react";
 import { Button } from "react-native-elements";
+
+export default () => {
+    return (
+      <Button
+        buttonStyle={{ width: 150 }}
+        containerStyle={{ margin: 5 }}
+        disabledStyle={{
+          borderWidth: 2,
+          borderColor: "#00F"
+        }}
+        disabledTitleStyle={{ color: "#00F" }}
+        loadingProps={{ animating: true }}
+        loadingStyle={{}}
+        onPress={() => alert("click")}
+        title="Log in"
+        titleProps={{}}
+        titleStyle={{ marginHorizontal: 5 }}
+      />
+    );
+  }
