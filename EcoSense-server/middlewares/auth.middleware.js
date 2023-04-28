d8c0b327245c29569e54222ea6996f4409347ed3
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-28 09:31:51 +0300

    Backend: Fetch user object from user table

--- EcoSense-server/middlewares/auth.middleware.js
+++ EcoSense-server/middlewares/auth.middleware.js
@@ -3,4 +3,5 @@ const User = require('../Models/user.model');
 const authMiddleware = async (req, res, next) => {
     const token = req.headers.authorization.split(' ')[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findOne({ email: decoded.email });
 };
