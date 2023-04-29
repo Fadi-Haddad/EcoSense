235025785de87819e04bdcaaf9bf5148512cb7ab
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-30 01:05:11 +0300

    Frontend: Remove unnecessary calls from App.tsx

--- EcoSense-mobile/EcoSense/App.tsx
+++ EcoSense-mobile/EcoSense/App.tsx
@@ -1,11 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image} from 'react-native';
-import LoginButton from './src/components/LoginButton';
-import CoverPicture from './src/components/CoverPicture';
-import EmailInput from './src/components/EmailInput';
-import PasswordInput from './src/components/PasswordInput';
 import LoginScreen from './screens/login';
-
 
 export default function App() {
   return (
@@ -13,12 +6,3 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: '#F4EEF2',
-    alignItems: 'center',
-    paddingTop:12,
-  },
-});
