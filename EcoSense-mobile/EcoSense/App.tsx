daabd1d9a249cdce5044c5bd3ddbecc7d311d50f
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-29 11:45:04 +0300

    Frontend: Create a different folder for cover picture

--- EcoSense-mobile/EcoSense/App.tsx
+++ EcoSense-mobile/EcoSense/App.tsx
@@ -2,14 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import LoginButton from './src/components/LoginButton';
 
-
 export default function App() {
   return (
     <View style={styles.container}>
-    <Image source={require('./assets/background.png')} style={{width: 100, height:100}}/>
-      <LoginButton 
-      title='Log in'
-      onPress={() => alert('Button clicked!')}/>
+      <Image source={require('./assets/background.png')} style={styles.image}/>
+      <View style={styles.buttonContainer}>
+        <LoginButton 
+          title='Log in'
+          onPress={() => alert('Button clicked!')}/>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,8 +19,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    // marginBottom:200,
+  },
+  image: {
+    flex: 0.40,
+    width: '100%',
+    height:'100%',
+    // resizeMode: 'cover',
+  },
+  buttonContainer: {
+    // marginTop:100,
   },
 });
+
