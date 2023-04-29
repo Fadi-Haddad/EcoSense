d43163c34447c7c3e704def86afac22b33410b3d
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-30 00:44:39 +0300

    Frontend: Create styles file for login page in login folder

---
+++ EcoSense-mobile/EcoSense/screens/login/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: 'column',
+      backgroundColor: '#F4EEF2',
+      alignItems: 'center',
+      paddingTop:12,
+    },
+  });
+
+  export default styles;
