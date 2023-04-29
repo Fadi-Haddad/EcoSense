d704429536d5cda453bda7f03c693ed72b7a62df
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-29 16:18:38 +0300

    Frontend: Add bottom padding to password field and move login button

--- EcoSense-mobile/EcoSense/App.tsx
+++ EcoSense-mobile/EcoSense/App.tsx
@@ -9,18 +9,18 @@ export default function App() {
   return (
     <View style={styles.container}>
        <CoverPicture />
-      <View>
-        <LoginButton 
-          title='Log in'
-          onPress={() => alert('Button clicked!')}/>
-      </View>
       <View style={{flexDirection:'row',paddingHorizontal:54,paddingTop:20}}>
       <MaterialIcons name='alternate-email' size={20} color="#666" style={{marginRight:5}}/>
       <TextInput placeholder='Email' style={{ flex:1, borderBottomWidth:1, borderBottomColor:'#ccc', width: '90%', paddingVertical:0}}/>
       </View>
-      <View style={{flexDirection:'row',paddingHorizontal:54,paddingTop:20}}>
+      <View style={{flexDirection:'row',paddingHorizontal:54,paddingTop:20,paddingBottom:30}}>
       <Ionicons name='ios-lock-closed-outline' size={20} color="#666" style={{marginRight:5}}/>
       <TextInput placeholder='password' style={{ flex:1, borderBottomWidth:1, borderBottomColor:'#ccc', width: '90%', paddingVertical:0}} secureTextEntry={true}/>
+      </View>
+      <View>
+        <LoginButton 
+          title='Log in'
+          onPress={() => alert('Button clicked!')}/>
       </View>
       <StatusBar style="auto" />
     </View>
