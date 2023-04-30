8ef6ea3ab08120422acc059d90b3150f9c31e960
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-30 15:10:25 +0300

    Frontend: Display second  row of the sensors in the container

--- EcoSense-mobile/EcoSense/screens/home/index.tsx
+++ EcoSense-mobile/EcoSense/screens/home/index.tsx
@@ -5,12 +5,20 @@ const Homescreen = ()=>{
   return(
     <View style={styles.container}>
       <View style={styles.row}>
-      <View style={styles.cell}>
-    <SensorCard name="Sensor 1" reading="25°C" state="OK" />
+        <View style={styles.cell}>
+          <SensorCard name="Sensor 1" reading="25°C" state="OK" />
+        </View>
+        <View style={styles.cell}>
+          <SensorCard name="Sensor 2" reading="20°C" state="OK" />
+        </View>
       </View>
-      <View style={styles.cell}>
-    <SensorCard name="Sensor 2" reading="30°C" state="OK" />
-      </View>
+      <View style={styles.row}>
+        <View style={styles.cell}>
+          <SensorCard name="Sensor 3" reading="30°C" state="OK" />
+        </View>
+        <View style={styles.cell}>
+          <SensorCard name="Sensor 4" reading="35°C" state="OK" />
+        </View>
       </View>
     </View>
   )
