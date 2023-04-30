95222d7edbeabbb1f1228180a7b2ff4331785963
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-30 08:45:36 +0300

    Frontend: Add a variable to represent login status for user

--- EcoSense-mobile/EcoSense/App.tsx
+++ EcoSense-mobile/EcoSense/App.tsx
@@ -1,4 +1,3 @@
-import LoginScreen from './screens/login';
 import OnboardingStack from './navigator/onboardingStack';
 
 export default function App() {
--- EcoSense-mobile/EcoSense/navigator/onboardingStack.tsx
+++ EcoSense-mobile/EcoSense/navigator/onboardingStack.tsx
@@ -7,6 +7,7 @@ import HomeScreen from '../screens/home';
 
 const OnboardingStack =() =>{
     const Stack = createStackNavigator ()
+    const isSignedIn = false; // hardcoded variable for testing
     return (
         <NavigationContainer>
         <Stack.Navigator>
