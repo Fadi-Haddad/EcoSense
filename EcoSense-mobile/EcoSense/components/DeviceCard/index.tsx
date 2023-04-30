90362a1f4a88fe992c2becebed9f7c7487c82955
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-30 20:03:30 +0300

    Frontend: Add the second item (fan status)

--- EcoSense-mobile/EcoSense/components/DeviceCard/index.tsx
+++ EcoSense-mobile/EcoSense/components/DeviceCard/index.tsx
@@ -3,16 +3,22 @@ import { View, Text, Image, StyleSheet } from 'react-native';
 
 interface Props{
     heaterStatus: string;
+    fanStatus: string;
 }
 
 
-const deviceCard = ({heaterStatus}:Props) => {
+const deviceCard = ({heaterStatus,fanStatus}:Props) => {
     return (
       <View>
         <View>
           <Text >{`Heater is ${heaterStatus}`}</Text>
           <Image source={heaterStatus === 'on' ? require('../../assets/heater.png') : require('../../assets/heater.png')} style={styles.image} />
         </View>
+        <View>
+          <Text >{`Fan is ${fanStatus}`}</Text>
+          <Image source={fanStatus === 'on' ? require('../../assets/fan.png') : require('../../assets/fan.png')} style={styles.image} />
+        </View>
       </View>
     );
   };
+
