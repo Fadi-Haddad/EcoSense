fc7836014f22b6a7942dc1cb6b6baca21c5ed814
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-30 14:58:14 +0300

    Frontend: Create container for sensors-grid element

--- EcoSense-mobile/EcoSense/screens/home/styles.ts
+++ EcoSense-mobile/EcoSense/screens/home/styles.ts
@@ -1,7 +1,13 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F2F2F2',
+  },
   });
 
   export default styles;
