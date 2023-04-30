b59a2e041d20dfa40c1356c977bd6894e49201ff
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-30 14:09:17 +0300

    Frontend: Define a style for card container

---
+++ EcoSense-mobile/EcoSense/screens/sensorcard/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#FD6B68',
+        borderRadius: 10,
+        padding: 16,
+        marginVertical: 8,
+        alignItems: 'center',
+        width: '40%',
+    },
+    });
+
+  export default styles;
