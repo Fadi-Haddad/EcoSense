c90be5d954e42fb972df872aa4109b7566828117
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-30 14:03:20 +0300

    Frontend: Import usetheme

--- EcoSense-mobile/EcoSense/screens/sensorcard/index.tsx
+++ EcoSense-mobile/EcoSense/screens/sensorcard/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { useTheme } from '@react-navigation/native';
 interface Props {
     name: string;
     reading: string;
