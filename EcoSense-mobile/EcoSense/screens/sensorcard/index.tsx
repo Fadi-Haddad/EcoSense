e8c20b25cd423a3caba97a608c1df77b8dbd46b5
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-30 14:07:11 +0300

    Frontend::art: Define a list of colors inside the card

--- EcoSense-mobile/EcoSense/screens/sensorcard/index.tsx
+++ EcoSense-mobile/EcoSense/screens/sensorcard/index.tsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { useTheme } from '@react-navigation/native';
+
+
+const customcolors = {
+  red: '#ff0000',
+  green: '#00ff00',
+  white: '#ffffff'
+};
 interface Props {
-    name: string;
-    reading: string;
-    state: string;
-  }
-const SensorCard = ({ name, reading, state }:Props) => {
-    const { colors } = useTheme();
-    return (
-      <View style={styles.container}>
-        <Text style={styles.name}>{name}</Text>
-        <Text style={styles.reading}>{reading}</Text>
-        <View style={styles.state}>
+  name: string;
+  reading: string;
+  state: string;
+}
+const SensorCard = ({ name, reading, state }: Props) => {
+  const { colors } = useTheme();
+  return (
+    <View style={styles.container}>
+      <Text style={styles.name}>{name}</Text>
+      <Text style={styles.reading}>{reading}</Text>
+      <View style={styles.state}>
         <Text style={[styles.stateText, { backgroundColor: customcolors.white }]}>{state}</Text>
-        </View>
       </View>
-    );
-  };
-  export default SensorCard;
+    </View>
+  );
+};
+
+export default SensorCard;
