81ba1eb2887a9e4558bec1239c57bb7c87742280
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-04-30 14:46:25 +0300

    Frontend: Display sensor card in home screen

--- EcoSense-mobile/EcoSense/screens/home/index.tsx
+++ EcoSense-mobile/EcoSense/screens/home/index.tsx
@@ -1,4 +1,10 @@
+import { View } from 'react-native';
+import SensorCard from '../sensorcard';
 const Homescreen = ()=>{
-  return null;
+  return(
+    <View>
+    <SensorCard name="Sensor 1" reading="25°C" state="OK" />
+    </View>
+  )
 }
 export default Homescreen;
