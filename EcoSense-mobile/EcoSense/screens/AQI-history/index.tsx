11de8505aa438ae5e707854bd8908f0d228eaaff
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-05-01 22:40:45 +0300

    Frontend: Add AQIGauge and AQIChart components to AQI history screen

--- EcoSense-mobile/EcoSense/screens/AQI-history/index.tsx
+++ EcoSense-mobile/EcoSense/screens/AQI-history/index.tsx
@@ -1,2 +1,12 @@
+import { View } from "react-native";
 import AQIChart from "../../components/AQIChart";
 import AQIGauge from "../../components/AQIGauge";
+
+const AQIhistory =()=>{
+    return(
+        <View>
+           <AQIGauge />
+           <AQIChart />
+        </View>
+    )
+}
