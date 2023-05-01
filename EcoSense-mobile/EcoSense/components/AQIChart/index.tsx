a85167f0630ae5f906e490c49899b2205e4ab3e2
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-05-01 14:50:31 +0300

    Frontend: Add random data to fill the chart

--- EcoSense-mobile/EcoSense/components/AQIChart/index.tsx
+++ EcoSense-mobile/EcoSense/components/AQIChart/index.tsx
@@ -3,6 +3,21 @@ import { Chart, Line, Area, HorizontalAxis, VerticalAxis } from 'react-native-re
 const MyChart = ()=>{
     return (
       <Chart
+      data={[
+        { x: -2, y: 15 },
+        { x: -1, y: 10 },
+        { x: 0, y: 12 },
+        { x: 1, y: 7 },
+        { x: 2, y: 6 },
+        { x: 3, y: 8 },
+        { x: 4, y: 10 },
+        { x: 5, y: 8 },
+        { x: 6, y: 12 },
+        { x: 7, y: 14 },
+        { x: 8, y: 12 },
+        { x: 9, y: 13 },
+        { x: 10, y: 18 },
+      ]}
         padding={{ left: 40, bottom: 20, right: 20, top: 20 }}
         xDomain={{ min: -2, max: 10 }}
         yDomain={{ min: 0, max: 20 }}
