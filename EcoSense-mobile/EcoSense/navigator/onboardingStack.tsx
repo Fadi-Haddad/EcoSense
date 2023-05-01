47c32caf1c4a4ae43c0253011e47f367e0493b11
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-05-01 23:23:22 +0300

    Frontend:art: Add styling to AQIhistory screen

--- EcoSense-mobile/EcoSense/navigator/onboardingStack.tsx
+++ EcoSense-mobile/EcoSense/navigator/onboardingStack.tsx
@@ -12,7 +12,7 @@ const OnboardingStack =() =>{
     const isSignedIn = true; // hardcoded variable for testing
     return (
         <NavigationContainer>
-        <Stack.Navigator initialRouteName={!isSignedIn ? "login" : "home"}>
+        <Stack.Navigator initialRouteName={!isSignedIn ? "login" : "AQIhistory"}>
         <Stack.Screen name="login" component={LoginScreen} options={{ headerShown: false }}/>
         <Stack.Screen name="home" component={HomeScreen} options={{ headerShown: false }}/>
         <Stack.Screen name="AQIhistory" component={AQIhistory} options={{ headerShown: false }}/>
--- EcoSense-mobile/EcoSense/screens/AQI-history/index.tsx
+++ EcoSense-mobile/EcoSense/screens/AQI-history/index.tsx
@@ -1,13 +1,24 @@
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 import AQIChart from "../../components/AQIChart";
 import AQIGauge from "../../components/AQIGauge";
 
 const AQIhistory =()=>{
     return(
-        <View>
+        <View style={styles.container}>
            <AQIGauge />
            <AQIChart />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 50,
+    marginTop: 40,
+  },
+});
+
 export default AQIhistory;
