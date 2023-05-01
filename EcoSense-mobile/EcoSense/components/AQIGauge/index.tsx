c25d0c93645703e9b192566ec59dd37297f999ac
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-05-01 21:15:53 +0300

    Frontend: Pass parameters to AnimatedCircularProgress node

--- EcoSense-mobile/EcoSense/components/AQIGauge/index.tsx
+++ EcoSense-mobile/EcoSense/components/AQIGauge/index.tsx
@@ -6,7 +6,12 @@ export default class AQIGauge extends Component {
     state = {fill: 70,};
     render() {
       return (
-        <AnimatedCircularProgress>
+        <AnimatedCircularProgress
+          size={200}
+          width={20}
+          fill={this.state.fill}
+          tintColor="#00e0ff"
+          backgroundColor="#3d5875">
           {(fill) => (
       <View style={{ alignItems: 'center'}}>
                 <Text style={{ fontSize: 50, fontWeight: 'bold' }}>
