f306b3f775c349e556de9e6b5e74cdaff558d4af
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-05-01 17:00:42 +0300

    Frontend: Add styling for component

--- EcoSense-mobile/EcoSense/components/CurrentReading/index.tsx
+++ EcoSense-mobile/EcoSense/components/CurrentReading/index.tsx
@@ -2,5 +2,40 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 const CurrentReading = () => {
-    return null;
+    const aqi = 5;
+    const state = 'fair';
+    return (
+      <View style={styles.container}>
+        <View>
+          <Text style={styles.number}>Air Quality:</Text>
+        </View>
+        <View>
+          <Text style={styles.number}>{aqi}</Text>
+        </View>
+        <Text style={styles.state}>{state}</Text>
+      </View>
+    );
   };
+
+  const styles = StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+      justifyContent:'space-around',
+      backgroundColor: '#FD6B68',
+      borderRadius:20,
+      paddingVertical:5,
+      paddingHorizontal:10,
+      marginBottom: 5,
+      width:'88%',
+    },
+    number: {
+      fontWeight: 'bold',
+      fontSize: 25,
+      color: 'black',
+    },
+    state: {
+      fontWeight: 'bold',
+      fontSize: 25,
+      color: 'black',
+    },
+  });
