2e4ebc1317e605f736e08e7f55a16b4c47058ce4
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-05-01 15:59:50 +0300

    Frontend: Remove floating point from Y axis on chart

--- EcoSense-mobile/EcoSense/components/AQIChart/index.tsx
+++ EcoSense-mobile/EcoSense/components/AQIChart/index.tsx
@@ -20,10 +20,11 @@ const AQIChart = ()=>{
         { x: 10, y: 18 },
       ]}
         padding={{ left: 40, bottom: 20, right: 20, top: 20 }}
-        xDomain={{ min: -2, max: 10 }}
+        xDomain={{ min: 0, max: 10 }}
         yDomain={{ min: 0, max: 20 }}
       >
-        <VerticalAxis tickCount={11} theme={{ labels: { formatter: (v) => v.toFixed(2) } }} />
+        <VerticalAxis tickCount={11} 
+        theme={{ labels: { formatter: (v) => v.toFixed(0) } }} />
         <HorizontalAxis tickCount={5} />
         <Area theme={{ gradient: { from: { color: '#fff000' }, to: { color: '#fff000', opacity: 0.4 } }}} />
         <Line theme={{ stroke: { color: '#fff999', width: 5 }, scatter: { default: { width: 4, height: 4, rx: 2 }} }} />
