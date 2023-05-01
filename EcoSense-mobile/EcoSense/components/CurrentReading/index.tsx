fd5804c486f7813e38015d96b9631bf325c07059
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-05-01 16:57:40 +0300

    Frontend : Create layout for AQI reading component

--- EcoSense-mobile/EcoSense/components/CurrentReading/index.tsx
+++ EcoSense-mobile/EcoSense/components/CurrentReading/index.tsx
@@ -1,48 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-interface Props {
-    aqi: number;
-    state: string;
-  }
-
-const CurrentReading = ({ aqi, state }:Props) => {
-  return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.number}>Air Quality:</Text>
-      </View>
-      <View>
-        <Text style={styles.number}>{aqi}</Text>
-      </View>
-      <Text style={styles.state}>{state}</Text>
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    justifyContent:'space-around',
-    backgroundColor: '#FD6B68',
-    borderRadius:20,
-    paddingVertical:5,
-    paddingHorizontal:10,
-    marginBottom: 5,
-    width:'88%',
-  },
-  number: {
-    fontWeight: 'bold',
-    fontSize: 25,
-    color: 'black',
-    // paddingHorizontal:10,
-  },
-  state: {
-    fontWeight: 'bold',
-    fontSize: 25,
-    color: 'black',
-    // paddingHorizontal:10,
-  },
-});
-
-export default CurrentReading;
+const CurrentReading = () => {
+    return null;
+  };
