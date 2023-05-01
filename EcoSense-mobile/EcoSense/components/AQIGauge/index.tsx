5c31b8f5bd39969917c5a963088c5836bc1a9c91
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-05-01 21:09:11 +0300

    Frontend: Create AQIGauge component

--- EcoSense-mobile/EcoSense/components/AQIGauge/index.tsx
+++ EcoSense-mobile/EcoSense/components/AQIGauge/index.tsx
@@ -1,3 +1,20 @@
 import React, { Component } from 'react';
 import { AnimatedCircularProgress } from 'react-native-circular-progress';
 import { View, Text, StyleSheet } from 'react-native';
+
+export default class AQIGauge extends Component {
+    state = {fill: 70,};
+    render() {
+      return (
+        <AnimatedCircularProgress>
+          {(fill) => (
+          <View style={{ alignItems: 'center'}}>
+                <Text>
+                  { this.state.fill }
+                </Text>
+              </View>
+            )}
+        </AnimatedCircularProgress>
+      );
+    }
+  }
