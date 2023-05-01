6e5475a3f7c8a6e11fc917f1f57b34027de479aa
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-05-01 23:48:44 +0300

    Frontend::wrench: Add AppBar component

--- EcoSense-mobile/EcoSense/screens/AQI-history/index.tsx
+++ EcoSense-mobile/EcoSense/screens/AQI-history/index.tsx
@@ -1,12 +1,16 @@
 import { View, StyleSheet } from "react-native";
 import AQIChart from "../../components/AQIChart";
 import AQIGauge from "../../components/AQIGauge";
+import { AppBar } from "@react-native-material/core";
 
 const AQIhistory =()=>{
     return(
         <View style={styles.container}>
-           <AQIGauge />
-           <AQIChart />
+            <AppBar title="Air Quality History" />
+            <View style={styles.content}>
+                <AQIGauge />
+                <AQIChart />
+            </View>
         </View>
     )
 }
@@ -14,10 +18,12 @@ const AQIhistory =()=>{
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop:34
+  },
+  content: {
+    flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    marginVertical: 50,
-    marginTop: 40,
   },
 });
 
