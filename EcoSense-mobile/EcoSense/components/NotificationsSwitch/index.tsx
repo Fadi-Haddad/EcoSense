84276a9ba333d6e95bf96020e6376234b85e7cc3
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-05-02 14:30:03 +0300

    Frontend: Add a text and a switch to the component

--- EcoSense-mobile/EcoSense/components/NotificationsSwitch/index.tsx
+++ EcoSense-mobile/EcoSense/components/NotificationsSwitch/index.tsx
@@ -1,3 +1,14 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { Stack, Switch } from "@react-native-material/core";
+
+const NotificationSwitch = () => {
+    const [checked, setChecked] = useState(true);
+  
+    return (
+      <View >
+        <Text>Enable Notifications</Text>
+        <Switch value={checked} onValueChange={() => setChecked(!checked)} />
+      </View>
+    );
+  };
