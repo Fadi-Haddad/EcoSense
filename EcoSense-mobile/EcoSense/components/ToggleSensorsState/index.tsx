15ceb3080820e96f09901d62969b5cfa7e02a374
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-05-02 23:52:22 +0300

    Frontend::heavy_plus_sign: Add toggleState function

--- EcoSense-mobile/EcoSense/components/ToggleSensorsState/index.tsx
+++ EcoSense-mobile/EcoSense/components/ToggleSensorsState/index.tsx
@@ -1,15 +1,22 @@
 import React, { useState } from 'react';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
 
-const ToggleSensorsState = () =>{
-    const sensorsOn = true;
-  return (
-    <View>
-        <Image
-          source={sensorsOn ? require('../../assets/sensors-on.png') : require('../../assets/sensors-off.png')}
-          style={{ width: 120, height: 120 }}
-        />
-      <Text>{sensorsOn ? 'Sensors are on' : 'Sensors are off'}</Text>
-    </View>
-  );
-};
+const ToggleSensorsState = () => {
+    const [sensorsOn, setSensorsOn] = useState(false);
+  
+    const toggleState = () => {
+      setSensorsOn(!sensorsOn);
+    };
+  
+    return (
+      <View>
+        <TouchableOpacity onPress={toggleState}>
+          <Image
+            source={sensorsOn ? require('../../assets/sensors-on.png') : require('../../assets/sensors-off.png')}
+            style={{ width: 120, height: 120 }}
+          />
+        </TouchableOpacity>
+        <Text>{sensorsOn ? 'Sensors are on' : 'Sensors are off'}</Text>
+      </View>
+    );
+  };
