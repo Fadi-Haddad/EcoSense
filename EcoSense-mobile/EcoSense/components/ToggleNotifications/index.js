8454c79fee67286e3f9b730ec16bba3f501fdc41
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-05-02 19:12:20 +0300

    Frontend: Define required hooks

--- EcoSense-mobile/EcoSense/components/ToggleNotifications/index.js
+++ EcoSense-mobile/EcoSense/components/ToggleNotifications/index.js
@@ -1,3 +1,7 @@
 import React, { useState } from "react";
 import { View, Text, TextInput, StyleSheet } from "react-native";
 import { Switch } from "@react-native-material/core";
+
+const [checked, setChecked] = useState(true);
+const [minValue, setMinValue] = useState("");
+const [maxValue, setMaxValue] = useState("");
