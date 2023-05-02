382eaa1d9a9b883f0284e6730f23fda7e75db6d1
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-05-02 19:13:25 +0300

    Frontend: Define handleswitch function

--- EcoSense-mobile/EcoSense/components/ToggleNotifications/index.js
+++ EcoSense-mobile/EcoSense/components/ToggleNotifications/index.js
@@ -2,6 +2,13 @@ import React, { useState } from "react";
 import { View, Text, TextInput, StyleSheet } from "react-native";
 import { Switch } from "@react-native-material/core";
 
-const [checked, setChecked] = useState(true);
-const [minValue, setMinValue] = useState("");
-const [maxValue, setMaxValue] = useState("");
+const Settings = () => {
+    const [checked, setChecked] = useState(true);
+    const [minValue, setMinValue] = useState("");
+    const [maxValue, setMaxValue] = useState("");
+  
+    const handleSwitch  = () => {
+      setChecked(!checked);
+    };
+
+}
