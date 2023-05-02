b6fe867ac34380400f7c58f9f67cca48313d7739
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-05-02 21:36:47 +0300

    Frontend: Add AppBar to settings screen

--- EcoSense-mobile/EcoSense/screens/settings/index.tsx
+++ EcoSense-mobile/EcoSense/screens/settings/index.tsx
@@ -4,6 +4,7 @@ import ToggleCONotifications from '../../components/ToggleCONotifications';
 import ToggleCO2Notifications from '../../components/ToggleCO2Notifications';
 import ToggleTempNotifications from '../../components/ToggleTempNotifications';
 import ToggleHumidityNotifications from '../../components/ToggleHumidityNotifications';
+import { AppBar } from "@react-native-material/core";
 const settings = ()=>{
     return (
         <View>
