ba0296be99badbaa403ce53b17f4f99ad3b47a66
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-05-02 15:08:57 +0300

    Frontend: Import NotificationSwitch in AQIHistory screen

--- EcoSense-mobile/EcoSense/screens/AQI-history/index.tsx
+++ EcoSense-mobile/EcoSense/screens/AQI-history/index.tsx
@@ -2,6 +2,7 @@ import { View, StyleSheet } from "react-native";
 import AQIChart from "../../components/AQIChart";
 import AQIGauge from "../../components/AQIGauge";
 import HighLow from "../../components/HighLow";
+import NotificationSwitch from "../../components/NotificationsSwitch";
 import { AppBar } from "@react-native-material/core";
 
 const AQIhistory =()=>{
@@ -12,6 +13,7 @@ const AQIhistory =()=>{
                 <AQIGauge />
                 <AQIChart />
                 <HighLow lowestReading={0} highestReading={42} lowestTimestamp="2022-08-01 13:15:14" highestTimestamp="2022-01-020 04:15:45"/>
+                <NotificationSwitch />
             </View>
         </View>
     )
