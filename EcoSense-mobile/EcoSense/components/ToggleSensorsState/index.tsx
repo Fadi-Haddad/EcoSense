b345b0b2bfcd385a866d4958bd4cf1015cbd0110
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-05-02 23:48:16 +0300

    Frontend: Design ToggleSensorsState component

--- EcoSense-mobile/EcoSense/components/ToggleSensorsState/index.tsx
+++ EcoSense-mobile/EcoSense/components/ToggleSensorsState/index.tsx
@@ -1,2 +1,15 @@
 import React, { useState } from 'react';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
+
+const ToggleSensorsState = () =>{
+    const sensorsOn = true;
+  return (
+    <View>
+        <Image
+          source={sensorsOn ? require('../../assets/sensors-on.png') : require('../../assets/sensors-off.png')}
+          style={{ width: 120, height: 120 }}
+        />
+      <Text>{sensorsOn ? 'Sensors are on' : 'Sensors are off'}</Text>
+    </View>
+  );
+};
