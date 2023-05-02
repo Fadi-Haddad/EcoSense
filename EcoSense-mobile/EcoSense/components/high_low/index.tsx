2f337c39c926d3711c3d45acc6e932a4488ba1f2
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-05-02 11:50:43 +0300

    Frontend: Define component struture

--- EcoSense-mobile/EcoSense/components/high_low/index.tsx
+++ EcoSense-mobile/EcoSense/components/high_low/index.tsx
@@ -1,3 +1,20 @@
 
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+
+const HighLow = ({ lowestReading, highestReading, lowestTimestamp, highestTimestamp }: Props) => {
+    return (
+      <View >
+        <View>
+        <Text >Lowest Reading:</Text>
+        <Text >lowestReading</Text>
+        <Text >lowestTimestamp</Text>
+      </View>
+      <View >
+      <Text >Lowest Reading:</Text>
+        <Text >lowestReading</Text>
+        <Text >lowestTimestamp</Text>
+      </View>
+      </View>
+    );
+  };
