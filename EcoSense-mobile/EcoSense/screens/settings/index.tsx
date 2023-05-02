dc531050d3f3fe40eb3b0c4a6407aac66a1c545b
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-05-02 21:10:17 +0300

    Frontend: Import required components to setting screen

--- EcoSense-mobile/EcoSense/screens/settings/index.tsx
+++ EcoSense-mobile/EcoSense/screens/settings/index.tsx
@@ -1,4 +1,9 @@
 import ToggleNotifications from '../../components/ToggleNotifications'
+import ToggleAQINotifications from '../../components/ToggleNotifications';
+import ToggleCONotifications from '../../components/ToggleCONotifications';
+import ToggleCO2Notifications from '../../components/ToggleCO2Notifications';
+import ToggleTempNotifications from '../../components/ToggleTempNotifications';
+import ToggleHumidityNotifications from '../../components/ToggleHumidityNotifications';
 const settings = ()=>{
     return (
         <ToggleNotifications />
