8ac71aa35816513b86339f0e929292c0704d18ae
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-05-05 10:21:51 +0300

    Frontend: Add react-native-async-storage/async-storage and axios

--- EcoSense-mobile/EcoSense/screens/login/index.tsx
+++ EcoSense-mobile/EcoSense/screens/login/index.tsx
@@ -1,4 +1,33 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View, Image, ScrollView} from 'react-native';
+// import LoginButton from '../../src/components/LoginButton';
+// import CoverPicture from '../../src/components/CoverPicture';
+// import EmailInput from '../../src/components/EmailInput';
+// import PasswordInput from '../../src/components/PasswordInput';
+// import styles from './styles';
+
+// const LoginScreen = ()=>{
+//   const [email, setEmail] = useState("");
+//   const handleEmailChange = (text) => {
+//     setEmail(text)
+//   };
+//     return (
+//       <View style={styles.container}>
+//         <CoverPicture />
+//         <EmailInput handleEmailChange={handleEmailChange}/>
+//         <PasswordInput />
+//         <View>
+//           <LoginButton 
+//             title='Log in'
+//             onPress={() => alert(email)}/>
+//         </View>
+//         <StatusBar style="auto" />
+//       </View>)
+// }
+// export default LoginScreen;
+
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, ScrollView} from 'react-native';
 import LoginButton from '../../src/components/LoginButton';
@@ -6,23 +35,67 @@ import CoverPicture from '../../src/components/CoverPicture';
 import EmailInput from '../../src/components/EmailInput';
 import PasswordInput from '../../src/components/PasswordInput';
 import styles from './styles';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const LoginScreen = ()=>{
+const LoginScreen = ({ navigation }) => {
   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('')
+
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      const storedEmail = await AsyncStorage.getItem('email');
+      const storedPassword = await AsyncStorage.getItem('password');
+      if (storedEmail && storedPassword) {
+        setEmail(storedEmail);
+        setPassword(storedPassword);
+      }
+    };
+    checkLoginStatus();
+  }, []);
+
   const handleEmailChange = (text) => {
-    setEmail(text)
+    setEmail(text);
   };
-    return (
-      <View style={styles.container}>
-        <CoverPicture />
-        <EmailInput handleEmailChange={handleEmailChange}/>
-        <PasswordInput />
-        <View>
-          <LoginButton 
-            title='Log in'
-            onPress={() => alert(email)}/>
-        </View>
-        <StatusBar style="auto" />
-      </View>)
-}
+  const handlePasswordChange = (text) => {
+    setPassword(text);
+  };
+
+  const handleLogin = async () => {
+    if (!email || !password) {
+      setErrorMessage('Please enter both email and password.');
+    } else if (!email.includes('@')) {
+      setErrorMessage('Please enter a valid email address.');
+    } else if (password.length < 6) {
+      setErrorMessage('Password must be at least 6 characters long.');
+    } else {
+      setErrorMessage('');
+      // Store email and password in AsyncStorage
+      try {
+        await AsyncStorage.setItem('email', email);
+        await AsyncStorage.setItem('password', password);
+      } catch (error) {
+        console.log('Error storing data:', error);
+      }
+      // Navigate to the home screen
+      navigation.navigate('home');
+    }
+  };
+  
+  return (
+    <View style={styles.container}>
+      <CoverPicture />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <EmailInput handleEmailChange={handleEmailChange} email={email} />
+      <PasswordInput handlePasswordChange={handlePasswordChange} password={password} />
+      <View>
+        <LoginButton 
+          title='Log in'
+          onPress={handleLogin}/>
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  )
+};
+
 export default LoginScreen;
--- EcoSense-mobile/EcoSense/src/components/passwordInput/index.tsx
+++ EcoSense-mobile/EcoSense/src/components/passwordInput/index.tsx
@@ -1,15 +1,75 @@
-import React from 'react';
+// import React from 'react';
+// import { View, TextInput } from 'react-native';
+// import Ionicons from 'react-native-vector-icons/Ionicons';
+// import { styles } from './styles';
+
+// const PasswordInput= ()=> {
+//   return (
+//     <View style={styles.inputContainer}>
+//       <Ionicons name='ios-lock-closed-outline' size={20} color='#666' style={styles.icon} />
+//       <TextInput placeholder='password' style={styles.input} secureTextEntry={true} />
+//     </View>
+//   );
+// }
+
+// export default PasswordInput;
+
+// PasswordInput.js
+
+import React, { useState, useEffect } from 'react';
 import { View, TextInput } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { styles } from './styles';
 
-const PasswordInput= ()=> {
+const PasswordInput = ({ handlePasswordChange }) => {
+  const [password, setPassword] = useState('');
+
+  useEffect(() => {
+    const getPassword = async () => {
+      try {
+        const storedPassword = await AsyncStorage.getItem('password');
+        if (storedPassword !== null) {
+          setPassword(storedPassword);
+        }
+      } catch (e) {
+        console.log('Error reading password from AsyncStorage: ', e);
+      }
+    };
+    getPassword();
+  }, []);
+
+  const savePassword = async (text) => {
+    try {
+      await AsyncStorage.setItem('password', text);
+      console.log('Password saved successfully');
+    } catch (e) {
+      console.log('Error saving password to AsyncStorage: ', e);
+    }
+  };
+
+  const handlePasswordChangeWithSave = (text) => {
+    setPassword(text);
+    savePassword(text);
+    handlePasswordChange(text);
+  };
+
   return (
     <View style={styles.inputContainer}>
       <Ionicons name='ios-lock-closed-outline' size={20} color='#666' style={styles.icon} />
-      <TextInput placeholder='password' style={styles.input} secureTextEntry={true} />
+      <TextInput
+        placeholder='password'
+        style={styles.input}
+        secureTextEntry={true}
+        onChangeText={handlePasswordChangeWithSave}
+        value={password}
+      />
     </View>
   );
-}
+};
 
 export default PasswordInput;
+
+
+// EmailInput.js
+
