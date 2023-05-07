bc61d0d8e710ea7d3d6a081d1a9c52f8ea3b2344
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-05-07 19:58:47 +0300

    Frontend: Wrap sensors cards and 'Details' text with TouchableOpacity in home screen

--- EcoSense-mobile/EcoSense/screens/home/index.tsx
+++ EcoSense-mobile/EcoSense/screens/home/index.tsx
@@ -1,29 +1,15 @@
-import { View,Text } from 'react-native';
+import { View,Text,TouchableOpacity  } from 'react-native';
 import SensorCard from '../../components/SensorCard';
 import DeviceCard from '../../components/DeviceCard';
 import AQIGauge from '../../components/AQIGauge';
-import NavigationBar from '../NavigationBar';
 import { useState, useEffect } from 'react';
-import styles from './styles';
+// import styles from './styles';
+import styles from './styles'
 
-const Homescreen = ()=>{
-  const [Data, setData] = useState({
-    "readings": {
-      "_id": "",
-      "AQI": 0,
-      "CO": 0,
-      "CO2": 0,
-      "Temp": 0,
-      "Humidity": 0,
-      "timeStamp": "0",
-      "__v": 0,},
-    "state": {
-      "AQI": "",
-      "CO": "",
-      "CO2": "",
-      "Temp": "",
-      "Humidity": "",}
-  });
+const Homescreen = ({navigation})=>{
+  const [Data, setData] = useState({"readings": {"_id": "","AQI": 0,"CO": 0,"CO2": 0,"Temp": 0,"Humidity": 0,"timeStamp": "0", "__v": 0,},
+    "state": {"AQI": "","CO": "","CO2": "","Temp": "","Humidity": "",}});
+
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch('http://192.168.0.100:8000/data/get_all_readings');
@@ -42,22 +28,32 @@ const Homescreen = ()=>{
     <View style={styles.container}>
       <AQIGauge Reading={Data.readings.AQI} />
       <Text style={{fontSize: 24, fontWeight:'800'}}>{Data.state.AQI}</Text>
-      <Text style={styles.details}>{'Details >'}</Text>
+      <TouchableOpacity onPress={() => navigation.navigate('AQIhistory')}>
+        <Text style={styles.details}>{'Details >'}</Text>
+      </TouchableOpacity>
       <View style={styles.row}>
+        <TouchableOpacity onPress={() => navigation.navigate('COhistory')}>
         <View style={styles.cell}>
           <SensorCard name="CO Level :" reading={Data.readings.CO} state={Data.state.CO} unit = "PPM" />
         </View>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('CO2history')}>
         <View style={styles.cell}>
           <SensorCard name="CO2 Level :" reading={Data.readings.CO2} state={Data.state.CO2} unit = "PPM"/>
         </View>
+      </TouchableOpacity>
       </View>
       <View style={styles.row}>
+      <TouchableOpacity onPress={() => navigation.navigate('Temphistory')}>
         <View style={styles.cell}>
           <SensorCard name="Temperature :" reading={Data.readings.Temp} state={Data.state.Temp} unit = "°C"/>
         </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate('Humidityhistory')}>
         <View style={styles.cell}>
           <SensorCard name="Humidity :" reading={Data.readings.Humidity} state={Data.state.Humidity} unit = "%"/>
         </View>
+        </TouchableOpacity>
       </View>
       <View>
         <DeviceCard heaterStatus="On" fanStatus="Off"/>
