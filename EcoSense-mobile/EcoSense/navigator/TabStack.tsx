91dba906121dd4d8c0ae86e39fa669ed71bb99d4
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-05-07 18:15:48 +0300

    Froantend: Add custom icons for TabNavigator

--- EcoSense-mobile/EcoSense/navigator/TabStack.tsx
+++ EcoSense-mobile/EcoSense/navigator/TabStack.tsx
@@ -2,18 +2,18 @@ import { NavigationContainer } from "@react-navigation/native";
 import Homescreen from "../screens/home";
 import settings from "../screens/settings";
 import NotificationsScreen from "../screens/notifications/NotificationsScreen";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const TabStack = ()=> {
     const Tab = createBottomTabNavigator();
     return (
         <NavigationContainer>
-        <Tab.Navigator>
-            <Tab.Screen name="Home" component={Homescreen} />
-            <Tab.Screen name="Notifications" component={NotificationsScreen} />
-            <Tab.Screen name="Settings" component={settings} />
-        </Tab.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Homescreen} options={{ headerShown: false, tabBarIcon: ({ color, size }) => (<Ionicons name="home" color={color} size={size} />),}}/>
+        <Tab.Screen name="Notifications" component={NotificationsScreen} options={{ headerShown: false, tabBarIcon: ({ color, size }) => (<Ionicons name="notifications" color={color} size={size} />),}}/>
+        <Tab.Screen name="Settings" component={settings} options={{ headerShown: false, tabBarIcon: ({ color, size }) => (<Ionicons name="settings" color={color} size={size} />),}}/>
+      </Tab.Navigator>
         </NavigationContainer>
     );
 }
