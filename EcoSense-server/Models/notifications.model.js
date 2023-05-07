5398387d0a984c2c78dbf2e7c2fb08f5521ede53
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-05-08 01:34:58 +0300

    Backend: Create new schema for sensors notifications

--- EcoSense-server/Models/notifications.model.js
+++ EcoSense-server/Models/notifications.model.js
@@ -1,3 +1,18 @@
 const mongoose = require('mongoose');
 
-
+const notificationSchema = new mongoose.Schema({
+    sensorName: {
+      type: String,
+      required: true,
+      enum: ['AQI', 'CO', 'CO2', 'Temp', 'Humidity'],
+    },
+    thresholdCrossed: {
+      type: Boolean,
+      required: true,
+    },
+    timestamp: {
+      type: Date,
+      required: true,
+      default: Date.now,
+    },
+  });
