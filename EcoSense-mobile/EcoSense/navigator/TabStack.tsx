125c0e45a7e2afb879b26a124eb95b3c15277592
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-05-07 17:35:02 +0300

    Frontend: Import necessary libraries and components in TabStack

---
+++ EcoSense-mobile/EcoSense/navigator/TabStack.tsx
@@ -0,0 +1,5 @@
+import { NavigationContainer } from "@react-navigation/native";
+import Homescreen from "../screens/home";
+import settings from "../screens/settings";
+import NotificationsScreen from "../screens/notifications/NotificationsScreen";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
