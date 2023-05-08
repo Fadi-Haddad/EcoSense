bee332e3b92c0a4902334eb66eee8d45984de672
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-05-08 23:07:17 +0300

    Frontend::fishing_pole_and_fish:Add hooks to manage the state of components

--- EcoSense-mobile/EcoSense/screens/notifications/NotificationsScreen.tsx
+++ EcoSense-mobile/EcoSense/screens/notifications/NotificationsScreen.tsx
@@ -1,7 +1,23 @@
-import { Text } from "react-native"
-const NotificationsScreen = ()=>{
-    return (
-        <Text>Nootification screen</Text>
-    )
+import React, { useState, useEffect } from "react";
+import { View } from "react-native";
+import Notification from "../../components/Notification";
+import { AppBar } from "@react-native-material/core";
+
+const NotificationsScreen = () => {
+  const [notifications, setNotifications] = useState([]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetch("http://192.168.0.100:8000/data/get_notifications_list")
+        .then((response) => response.json())
+        .then((data) => {
+          setNotifications(data);
+        })
+        .catch((error) => console.log(error));
+    }, 10000);
+
+    return () => clearInterval(interval);
+  }, []);
+  return (null)
 }
-export default NotificationsScreen;
+  export default NotificationsScreen;
