5ec6c615e2eb561ced65da249a9cc8649d2700cf
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-05-08 10:06:13 +0300

    Backend::new: Define new function (setDeviceState) in devices controller

--- EcoSense-server/Controllers/devices.controller.js
+++ EcoSense-server/Controllers/devices.controller.js
@@ -1,24 +1,13 @@
 const sensorReading= require('../Models/sensor.readings.model');
 const Devices = require("../Models/devices.model");
+const Notifications = require ("../Models/notifications.model")
 const validDeviceNames = ['fan','heater'];
 const validDeviceStates = ['on', 'off'];
 const validOperationModes = ['auto', 'manual'];
 
-// const setDeviceState = async(req,res)=>{
-//     try{
-//     const device_state = req.params.state;
-//     const device_name = req.params.device;
-//     if(validDeviceNames.includes(device_name) && validDeviceStates.includes(device_state)){
-//         const updateState= await Devices.findOneAndReplace({name:device_name},{state:device_state}, {new:true});
-//         if(!updateState){
-//             return res.staus(400).send("Invalid device name or state")}
-//         res.send(updateState);}
-//         return res.staus(200).send("Device state changed sucessfully")
-//     } catch(err){
-//         console.log(err);
-//         res.status(400).json({ message: 'Error update devices state' });
-//     }
-// }
+const setDeviceState = ()=>{
+
+    }
 const setFanState = async (req, res) => {
     try{
     let fanState = req.params.fan_state;
