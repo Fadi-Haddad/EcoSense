de9a28952a948d3386002a8e9e8bd1869f123bb0
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-05-08 11:23:47 +0300

    Backend::new: Create a route for getDevicesState function

--- EcoSense-server/routes/device.route.js
+++ EcoSense-server/routes/device.route.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { setFanState,setHeaterState } = require("../Controllers/devices.controller");
+const { setFanState,setHeaterState,getDevicesState } = require("../Controllers/devices.controller");
 const router = express.Router();
 router.get("/set_fan_state/:fan_operation_mode/:fan_state", setFanState);
 router.get("/set_heater_state/:heater_operation_mode/:heater_state", setHeaterState);
+router.get("/get_devices_state", getDevicesState);
 
 module.exports  = router;
